ea15d8a1e95c7541aa3673ac735bee8579fd15d0
Biniam batu <Biniambatu12@gmail.com>
2024-02-06 18:22:16 +0300

    show _loading _skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 
+
 interface props {
   game: Game;
 }
 const GameCard = ({ game }: props) => {
  
   return (
-    <Card  borderRadius={10} overflow={"hidden"}>
+    <Card width='300px' borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize=" 2xl">{game.name}</Heading>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,16 +23,23 @@ const GameGrid = () => {
     
         const [games,setGames] = useState<Game[]>([]);
         const [error,setError] = useState(' ');
-        
+        const [isLoading, setLoading] = useState(false)
         useEffect(()=>{
-         const controller = new AbortController();   
-         apiClient.get<FetchGameResponse>('/games',{signal: controller.signal})
-          .then(res => setGames(res.data.results))
-          .catch(err => {
-            if(err instanceof CanceledError) return; 
-            setError(err.message)})
+         const controller = new AbortController();
+         setLoading(true)   
+         apiClient
+           .get<FetchGameResponse>('/games',{signal: controller.signal})
+           .then(res => {
+              setGames(res.data.results)
+              setLoading(false)  
+            })
+           .catch(err => {
+             if(err instanceof CanceledError) return; 
+             setError(err.message)
+             setLoading(false) 
+            })
         return () => controller.abort();
         },[])
-        return {games,error}
+        return {games,error,isLoading}
 }
 export default GameGrid
